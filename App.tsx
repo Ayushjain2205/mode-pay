fc751a6e15a7a5f7df131936c0067865b42a8906
Ayush Jain <ayushjain2205@gmail.com>
2023-12-15 00:57:19 +0530

    Add toast for txn pending and succcess

--- App.tsx
+++ App.tsx
@@ -1,3 +1,14 @@
+import React, { useState } from "react";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  useColorScheme,
+  View,
+} from "react-native";
+import { Colors } from "react-native/Libraries/NewAppScreen";
+import Toast from "react-native-toast-message";
 import {
   ConnectWallet,
   embeddedWallet,
@@ -9,9 +20,6 @@ import {
   walletConnect,
   useSDK,
 } from "@thirdweb-dev/react-native";
-import React, { useState } from "react";
-import { Button, StyleSheet, Text, useColorScheme, View } from "react-native";
-import { Colors } from "react-native/Libraries/NewAppScreen";
 
 const App = () => {
   return (
@@ -19,20 +27,12 @@ const App = () => {
       activeChain="mumbai"
       clientId={process.env.EXPO_PUBLIC_TW_CLIENT_ID}
       supportedWallets={[
-        metamaskWallet({
-          recommended: true,
-        }),
+        metamaskWallet({ recommended: true }),
         rainbowWallet(),
-        walletConnect({
-          recommended: true,
-        }),
+        walletConnect({ recommended: true }),
         embeddedWallet({
           auth: {
-            // you need to enable EmbeddedWallets under your API Key in your thirdweb dashboard:
-            // https://thirdweb.com/dashboard/settings/api-keys
             options: ["email", "google"],
-            // you need to add this deeplink in your allowed `Redirect URIs` under your API Key in your thirdweb dashboard:
-            // https://thirdweb.com/dashboard/settings/api-keys
             redirectUrl: "rnstarter://",
           },
         }),
@@ -40,6 +40,7 @@ const App = () => {
         localWallet(),
       ]}
     >
+      <Toast />
       <AppInner />
     </ThirdwebProvider>
   );
@@ -47,7 +48,7 @@ const App = () => {
 
 const AppInner = () => {
   const isDarkMode = useColorScheme() === "dark";
-  const [transferStatus, setTransferStatus] = useState("");
+  const [amount, setAmount] = useState("");
 
   const textStyles = {
     color: isDarkMode ? Colors.white : Colors.black,
@@ -58,27 +59,55 @@ const AppInner = () => {
 
   const handleTransfer = async () => {
     if (!sdk) {
-      setTransferStatus("SDK not initialized. Please connect a wallet.");
+      Toast.show({
+        type: "error",
+        text1: "SDK Initialization",
+        text2: "SDK not initialized. Please connect a wallet.",
+      });
       return;
     }
 
+    Toast.show({
+      type: "info",
+      text1: "Transaction Status",
+      text2: "Sending transaction...",
+      visibilityTime: 0,
+      autoHide: false,
+      position: "top",
+    });
+
     try {
-      const toAddress = "0xCafa93E9985793E2475bD58B9215c21Dbd421fD0"; // Replace with actual recipient address
-      const amount = "0.01"; // Replace with the amount to transfer
-      const data = await sdk.wallet.transfer(toAddress, amount);
-      setTransferStatus("Transfer successful!");
+      const toAddress = "0xCafa93E9985793E2475bD58B9215c21Dbd421fD0"; // Hardcoded recipient address
+      await sdk.wallet.transfer(toAddress, amount);
+      Toast.hide();
+      Toast.show({
+        type: "success",
+        text1: "Transaction Status",
+        text2: "Transfer successful!",
+      });
     } catch (error) {
       console.error("Transfer failed:", error);
-      setTransferStatus("Transfer failed. See console for details.");
+      Toast.hide();
+      Toast.show({
+        type: "error",
+        text1: "Transaction Status",
+        text2: "Transfer failed. See console for details.",
+      });
     }
   };
 
   return (
     <View style={styles.view}>
       <Text style={textStyles}>React Native thirdweb starter</Text>
-      <ConnectWallet />
+      <TextInput
+        style={styles.input}
+        onChangeText={setAmount}
+        value={amount}
+        placeholder="Amount to Transfer"
+        keyboardType="numeric"
+      />
       <Button title="Transfer" onPress={handleTransfer} />
-      <Text>{transferStatus}</Text>
+      <ConnectWallet />
     </View>
   );
 };
@@ -96,9 +125,12 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     marginBottom: 20,
   },
-  button: {
-    marginTop: 20,
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
     padding: 10,
+    color: "black", // Adjust color as needed
   },
 });
 
