1ed2f83e9c8a41c323b6816f0ef444fcad454906
Ayush Jain <ayushjain2205@gmail.com>
2023-12-14 18:40:53 +0530

    basic payment functionality

--- App.tsx
+++ App.tsx
@@ -7,9 +7,10 @@ import {
   ThirdwebProvider,
   trustWallet,
   walletConnect,
+  useSDK,
 } from "@thirdweb-dev/react-native";
-import React from "react";
-import { StyleSheet, Text, useColorScheme, View } from "react-native";
+import React, { useState } from "react";
+import { Button, StyleSheet, Text, useColorScheme, View } from "react-native";
 import { Colors } from "react-native/Libraries/NewAppScreen";
 
 const App = () => {
@@ -46,16 +47,38 @@ const App = () => {
 
 const AppInner = () => {
   const isDarkMode = useColorScheme() === "dark";
+  const [transferStatus, setTransferStatus] = useState("");
 
   const textStyles = {
     color: isDarkMode ? Colors.white : Colors.black,
     ...styles.heading,
   };
 
+  const sdk = useSDK();
+
+  const handleTransfer = async () => {
+    if (!sdk) {
+      setTransferStatus("SDK not initialized. Please connect a wallet.");
+      return;
+    }
+
+    try {
+      const toAddress = "0xCafa93E9985793E2475bD58B9215c21Dbd421fD0"; // Replace with actual recipient address
+      const amount = "0.01"; // Replace with the amount to transfer
+      const data = await sdk.wallet.transfer(toAddress, amount);
+      setTransferStatus("Transfer successful!");
+    } catch (error) {
+      console.error("Transfer failed:", error);
+      setTransferStatus("Transfer failed. See console for details.");
+    }
+  };
+
   return (
     <View style={styles.view}>
       <Text style={textStyles}>React Native thirdweb starter</Text>
       <ConnectWallet />
+      <Button title="Transfer" onPress={handleTransfer} />
+      <Text>{transferStatus}</Text>
     </View>
   );
 };
@@ -72,6 +95,10 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: "bold",
     marginBottom: 20,
+  },
+  button: {
+    marginTop: 20,
+    padding: 10,
   },
 });
 
